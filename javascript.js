23f3b740a1b1712e47c017ae6b0e57d43a545306
pedrohmtoledo <toledopedro.hmt@gmail.com>
2024-10-10 20:04:28 +0100

    implement the prompt button
    implemented the prompt button for a new grid size

--- javascript.js
+++ javascript.js
@@ -1,29 +1,56 @@
-let gridSize = 5;
+let startGridSize = 16;
 const container = document.querySelector("#container");
-let boxHeight = 100/gridSize;
+
 
 function loadGrid(gridSize){
 for (let i = 0; i < gridSize; i++){
+    let boxHeight = 100/gridSize;
     const newRow = document.createElement("div");
     container.appendChild(newRow);
-    newRow.setAttribute("style","color: blue; border: 1px solid; display: flex; gap: 1px; margin-bottom: 1px")
-    newRow.setAttribute("class", "row")
+    newRow.setAttribute("style","border: 1px solid; display: flex; gap: 1px");
+    newRow.setAttribute("class", "row");
     newRow.style.height = (boxHeight + "%");
 
     for (let j = 0; j < gridSize; j++){
         const newItem = document.createElement("div");
-        // newItem.textContent = j;
-        newItem.setAttribute("style","flex: 1; border: 1px solid")
-        newItem.setAttribute("class", "box")
-        newRow.appendChild(newItem)
+        newItem.setAttribute("style","flex: 1; border: 1px solid; margin: 1px");
+        newItem.setAttribute("class", "box");
+        newRow.appendChild(newItem);
     }
    
 }
 }
-
-loadGrid(gridSize)
-
+loadGrid(startGridSize)
 const box = document.querySelectorAll(".box").forEach((e) => e.addEventListener("mouseenter", paint));
+let newGridSize = 0;
+const btn = document.querySelector("button");
+btn.addEventListener("click", event => {
+    let newGridSize = prompt("Type in new grid size. From 1 to 100");
+    if (newGridSize === null){
+        return; 
+    }
+    else if (newGridSize > 100 || newGridSize < 1){
+        alert("Size now permited.");
+        newGridSize = prompt("Type in new grid size. From 1 to 100");
+    }
+    while(container.firstChild){
+        container.removeChild(container.firstChild)
+    }
+    loadGrid(newGridSize)
+    const box = document.querySelectorAll(".box").forEach((e) => e.addEventListener("mouseenter", paint));
+})
+
+ function takeNewGridSize() {
+    return newGridSize = prompt("Input new grid size");
+    loadGrid(newGridSize)
+ }
+
+ console.log(newGridSize)
+// while(gridSize > 100 || gridSize < 1){
+//     alert("grid size should be less than 100");
+//     gridSize = prompt("Input new grid size")
+// }
+
 
 function paint(a){
     a.currentTarget.style.backgroundColor = "black";
