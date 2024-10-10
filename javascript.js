7586029de457a92d888484f81fc48a3ea7f078c5
pedrohmtoledo <toledopedro.hmt@gmail.com>
2024-10-10 20:06:43 +0100

    final project done

--- javascript.js
+++ javascript.js
@@ -1,30 +1,20 @@
 let startGridSize = 16;
 const container = document.querySelector("#container");
 
-
-function loadGrid(gridSize){
-for (let i = 0; i < gridSize; i++){
-    let boxHeight = 100/gridSize;
-    const newRow = document.createElement("div");
-    container.appendChild(newRow);
-    newRow.setAttribute("style","border: 1px solid; display: flex; gap: 1px");
-    newRow.setAttribute("class", "row");
-    newRow.style.height = (boxHeight + "%");
-
-    for (let j = 0; j < gridSize; j++){
-        const newItem = document.createElement("div");
-        newItem.setAttribute("style","flex: 1; border: 1px solid; margin: 1px");
-        newItem.setAttribute("class", "box");
-        newRow.appendChild(newItem);
-    }
-   
-}
-}
-loadGrid(startGridSize)
-const box = document.querySelectorAll(".box").forEach((e) => e.addEventListener("mouseenter", paint));
 let newGridSize = 0;
 const btn = document.querySelector("button");
-btn.addEventListener("click", event => {
+
+loadGrid(startGridSize)
+const box = document.querySelectorAll(".box").forEach((e) => e.addEventListener("mouseenter", paint));
+btn.addEventListener("click", generateNewGrid)
+
+
+function paint(a){
+    a.currentTarget.style.backgroundColor = "black";
+    console.log("hovering")
+}
+
+function generateNewGrid() {
     let newGridSize = prompt("Type in new grid size. From 1 to 100");
     if (newGridSize === null){
         return; 
@@ -38,21 +28,23 @@ btn.addEventListener("click", event => {
     }
     loadGrid(newGridSize)
     const box = document.querySelectorAll(".box").forEach((e) => e.addEventListener("mouseenter", paint));
-})
+}
 
- function takeNewGridSize() {
-    return newGridSize = prompt("Input new grid size");
-    loadGrid(newGridSize)
- }
-
- console.log(newGridSize)
-// while(gridSize > 100 || gridSize < 1){
-//     alert("grid size should be less than 100");
-//     gridSize = prompt("Input new grid size")
-// }
-
-
-function paint(a){
-    a.currentTarget.style.backgroundColor = "black";
-    console.log("hovering")
+function loadGrid(gridSize){
+    for (let i = 0; i < gridSize; i++){
+        let boxHeight = 100/gridSize;
+        const newRow = document.createElement("div");
+        container.appendChild(newRow);
+        newRow.setAttribute("style","border: 1px solid; display: flex; gap: 1px");
+        newRow.setAttribute("class", "row");
+        newRow.style.height = (boxHeight + "%");
+    
+        for (let j = 0; j < gridSize; j++){
+            const newItem = document.createElement("div");
+            newItem.setAttribute("style","flex: 1; border: 1px solid; margin: 1px");
+            newItem.setAttribute("class", "box");
+            newRow.appendChild(newItem);
+        }
+       
+    }
 }
